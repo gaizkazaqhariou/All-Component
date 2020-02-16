9e59dd8899e5bace53c152d153fdbce423a797ec
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-02-16 15:42:24 +0700

    add item menu and sub item menu

--- app/src/main/java/id/ac/polinema/allcomponent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/allcomponent/MainActivity.java
@@ -1,6 +1,8 @@
 package id.ac.polinema.allcomponent;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.item_menu, menu);
+        return true;
     }
 }
